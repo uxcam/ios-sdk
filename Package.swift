f229e29f4a06c11429a4c7fc7b1c2f4515b1c072
UXCam <uxcam-mobile@uxcam.com>
2022-07-15 10:31:40 +0000

    Release 3.4.2 @ 071520221031

--- Package.swift
+++ Package.swift
@@ -1,7 +1,9 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let version = "3.4.1"
+let version = "3.4.2"
+let checksum = "dc4413d4b6b80ed50f4d9fa3c1c500a0005529052d0c4c69a2de3f18d4b74404"
+let repoName = "ios-sdk"
 
 let package = Package(
 
@@ -15,42 +17,39 @@ let package = Package(
     products:
     [
         .library(
-			name: "UXCam",
+            name: "UXCam",
             targets: ["UXCamWrapper", "UXCam"]
-		)
+        )
     ],
 
     targets:
     [
-		// 'UXCamWrapper' target is a way to include the necessary linkerSettings that the binary XCFramework in UXCamFW requires.
-		.target(
-				name: "UXCamWrapper",
-				path: "UXCamWrapper",
-				exclude: ["README.md"],
-				linkerSettings:
-				[
-					.linkedFramework("AVFoundation"),
-					.linkedFramework("CoreGraphics"),
-					.linkedFramework("CoreMedia"),
-					.linkedFramework("CoreVideo"),
-					.linkedFramework("CoreTelephony"),
-					.linkedFramework("MobileCoreServices"),
-					.linkedFramework("QuartzCore"),
-					.linkedFramework("Security"),
-					.linkedFramework("SystemConfiguration"),
-					.linkedFramework("WebKit"),
-					.linkedLibrary("z"),
-					.linkedLibrary("iconv")
-				]
-		),
+        // 'UXCamWrapper' target is a way to include the necessary linkerSettings that the binary XCFramework in UXCamFW requires.
+        .target(
+                name: "UXCamWrapper",
+                path: "UXCamWrapper",
+                exclude: ["README.md"],
+                linkerSettings:
+                [
+                    .linkedFramework("AVFoundation"),
+                    .linkedFramework("CoreGraphics"),
+                    .linkedFramework("CoreMedia"),
+                    .linkedFramework("CoreVideo"),
+                    .linkedFramework("CoreTelephony"),
+                    .linkedFramework("MobileCoreServices"),
+                    .linkedFramework("QuartzCore"),
+                    .linkedFramework("Security"),
+                    .linkedFramework("SystemConfiguration"),
+                    .linkedFramework("WebKit"),
+                    .linkedLibrary("z"),
+                    .linkedLibrary("iconv")
+                ]
+        ),
 
         .binaryTarget(
             name: "UXCam",
-            //path: "Framework/UXCam.xcframework"
-			//url: "https://github.com/uxcam/ios-sdk/UXCam.xcframework.zip",
-			url: "https://raw.githubusercontent.com/uxcam/ios-sdk/\(version)/UXCam.xcframework.zip",
-			checksum: "d6372328c214f5950c8a422c232f4062565296bec4e9db150d10191a86099c07"
-		)
+            url: "https://raw.githubusercontent.com/uxcam/\(repoName)/\(version)/UXCam.xcframework.zip",
+            checksum: checksum
+        )
     ]
 )
-
--- UXCam.podspec
+++ UXCam.podspec
@@ -1,30 +1,30 @@
 Pod::Spec.new do |s|
-  s.name     			= 'UXCam'
-  s.version  			= '3.4.1'
+  s.name                 = 'UXCam'
+  s.version              = '3.4.2'
   
-  s.summary 			= "UXCam SDK for iOS applications"
-  s.description 		= <<-DESC
+  s.summary             = "UXCam SDK for iOS applications"
+  s.description         = <<-DESC
                   Visit https://uxcam.com to get a key.
                   This is version #{s.version} of the SDK - see CHANGELOG for details
                   DESC
-  s.homepage 			= "https://uxcam.com/"
-  s.license      		= {
+  s.homepage             = "https://uxcam.com/"
+  s.license              = {
     'type' => 'Copyright',
-    'text' => 'Copyright (c) 2013-2021, UXCam Inc. All rights reserved.'
+    'text' => 'Copyright (c) 2013-2022, UXCam Inc. All rights reserved.'
   }
 
-  s.author   			= { 'UXCam' => 'admin@uxcam.com' }
-  s.social_media_url 	= "https://www.twitter.com/uxcam"
-  s.documentation_url 	= 'https://help.uxcam.com/hc/en-us/categories/360001001392-Tailor-for-Success'
+  s.author               = { 'UXCam' => 'admin@uxcam.com' }
+  s.social_media_url     = "https://www.twitter.com/uxcam"
+  s.documentation_url     = 'https://developer.uxcam.com/docs'
   
-  s.platform 			= :ios, 10.0
+  s.platform             = :ios, 10.0
 
-  s.source 				= { :http => "https://raw.githubusercontent.com/uxcam/ios-sdk/#{s.version}/UXCam.xcframework.zip" }
+  s.source                 = { :http => "https://raw.githubusercontent.com/uxcam/ios-sdk/#{s.version}/UXCam.xcframework.zip" }
   s.vendored_frameworks = 'UXCam.xcframework'
 
-  s.static_framework 	= true
-  s.libraries 			= 'z', 'iconv'
-  s.frameworks     		= 'AVFoundation', 'CoreGraphics', 'CoreMedia', 'CoreVideo', 'CoreTelephony', 'MobileCoreServices', 'QuartzCore', 'SystemConfiguration', 'Security', 'WebKit'
-  s.xcconfig       		= { 'FRAMEWORK_SEARCH_PATHS' => '"$(PODS_ROOT)/UXCam"' }
-  s.requires_arc		= true
+  s.static_framework     = true
+  s.libraries             = 'z', 'iconv'
+  s.frameworks             = 'AVFoundation', 'CoreGraphics', 'CoreMedia', 'CoreVideo', 'CoreTelephony', 'MobileCoreServices', 'QuartzCore', 'SystemConfiguration', 'Security', 'WebKit'
+  s.xcconfig               = { 'FRAMEWORK_SEARCH_PATHS' => '"$(PODS_ROOT)/UXCam"' }
+  s.requires_arc        = true
 end
